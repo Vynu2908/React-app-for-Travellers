708e48d8a6ce7181c2a9b938415543b9a5687365
Vynu2908 <64244062+Vynu2908@users.noreply.github.com>
2023-04-21 15:26:33 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container mt-5">
+      <h1>Welcome to Travellers App</h1>
+      <p>Plan your next adventure with us!</p>
+    </div>
+  );
+}
+
+export default Home;

951699d52c3f72dbbe77bd8b3b8462e01425bd34
Vynu2908 <64244062+Vynu2908@users.noreply.github.com>
2023-04-21 15:27:39 +0530

    Create destination.js

---
+++ destination.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Destinations() {
+  return (
+    <div className="container mt-5">
+      <h1>Destinations</h1>
+      <p>Explore the world with our curated destinations!</p>
+    </div>
+  );
+}
+
+export default Destinations;
